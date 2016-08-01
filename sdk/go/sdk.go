70b17818d1cac9e631a1a2396ece9e74e5a288b1
xuning <xuning@didichuxing.com>
2016-08-01 22:24:14 +0800

    打印签名后的信息

--- sdk/go/sdk.go
+++ sdk/go/sdk.go
@@ -97,9 +97,11 @@ func signedRequest(uri, publicKey, privateKey, userid string) map[string]string 
 
 	// calculate HMAC with SHA256 and base64-encoding
 	signature := computeHmac256(string_to_sign, privateKey)
+	fmt.Printf("string_to_sign:%s,privateKey:%s,signature:%s,", string_to_sign, privateKey, signature)
 
 	// encode the signature for the request
 	signature = url.QueryEscape(signature)
+	fmt.Printf("signature_escape:%s\n", signature)
 	params["signature"] = signature
 
 	return params
